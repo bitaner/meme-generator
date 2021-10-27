7439510dd7ad6efd3dd39b6670d23562ca380813
Aner Biton <aner.biton@gmail.com>
2021-10-27 20:07:38 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,49 @@
 'use strict'
 
-function onInit() {
-    console.log('hello onInit')
-}
+// -------------------global vars -----------------------
 
 var gKeywords = { 'happy': 12, 'funny puk': 1 }
-var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] }];
+var gImg
+var gCtx
+var gElCanvas
 
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [{
-        txt: 'I never eat Falafel',
-        size: 20,
-        align: 'left',
-        color: 'red'
-    }]
+//-------------------------------------------------------
+
+
+function onInit() {
+    console.log('hello onInit')
+    gElCanvas = document.getElementById('main-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    drawImg2()
 }
+
+
+//----------------------img input--------------------- 
+
+
+function drawImg2() {
+    var img = new Image();
+    img.src = `img/${gMeme.selectedImgId}.jpg`
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    };
+}
+
+// ---------------------- TEXT --------------------------
+
+function drawText() {
+    var text = gMeme.lines[0].txt
+    gCtx.font = '48px impact';
+    const x = gElCanvas.width / 2
+    const y = 50
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'brown';
+    gCtx.fillStyle = 'black';
+    gCtx.textAlign = 'center';
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+// Add text input to the HTML and dynamically take the text line value from 
+// the input to gMeme and from it to the Canvas
---
+++ js/services/memeService.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I never eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red'
+    }]
+}
+
+
+var gImgs = [
+    { id: 101, url: 'img/1.jpg', keywords: ['politics', 'angry', 'man', 'crazy', 'trump'] },
+    { id: 102, url: 'img/2.jpg', keywords: ['animals', 'cute', 'dog'] },
+    { id: 103, url: 'img/3.jpg', keywords: ['sleep', 'animals', 'baby', 'cute', 'dog'] },
+    { id: 104, url: 'img/4.jpg', keywords: ['sleep', 'animals', 'cute', 'cat'] },
+    { id: 105, url: 'img/5.jpg', keywords: ['baby', 'success', 'win'] },
+];
