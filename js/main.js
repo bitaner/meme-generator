f0cedd4c8248e9ca5d417c30e74781f0b58f1228
Aner Biton <aner.biton@gmail.com>
2021-10-28 03:06:24 +0300

    4th commit

--- js/main.js
+++ js/main.js
@@ -7,22 +7,24 @@ var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] }]; // didnt use yet
 var gImg
 var gCtx
 var gElCanvas
+var gCurrLine = 0
 
 //-------------------------------------------------------
 
 
 function onInit() {
-    console.log('hello onInit')
+    // console.log('hello onInit')
     renderCanvas()
 }
 
 
 // -------------------- RENDER CANVAS ------------------
 
+
 function renderCanvas() {
-    console.log('hello renderCanvas')
+    // console.log('hello renderCanvas')
     const meme = getMemeForDisplay()
-    console.log('meme', meme);
+        // console.log('meme', meme);
     gElCanvas = document.getElementById('main-canvas');
     gCtx = gElCanvas.getContext('2d');
     var img = new Image();
@@ -51,19 +53,49 @@ function drawImg() {
 // ---------------------- TEXT --------------------------
 
 function drawText(meme) {
-    var text = meme.lines[0].txt
+    var text = meme.lines[gCurrLine].txt
     if (!text) text = ''
-    gCtx.font = '48px impact';
+    gCtx.font = `${meme.lines[gCurrLine].size}px ${meme.lines[gCurrLine].font}`
     const x = gElCanvas.width / 2
-    const y = 50
-    gCtx.lineWidth = 2;
-    gCtx.strokeStyle = 'brown';
-    gCtx.fillStyle = 'black';
-    gCtx.textAlign = 'center';
+    const y = meme.lines[gCurrLine].lineHeight
+    gCtx.lineWidth = meme.lineWidth;
+    gCtx.strokeStyle = meme.lines[gCurrLine].margin
+    gCtx.fillStyle = meme.lines[gCurrLine].color
+    gCtx.textAlign = meme.lines[gCurrLine].align
     gCtx.fillText(text, x, y);
     gCtx.strokeText(text, x, y);
 }
 
+function onFontSize(diff) {
+    // console.log('hello onFontSize')
+    fontSize(diff)
+    renderCanvas()
+}
+
+function onLineHeight(diff) {
+    // console.log('hello onLineHeight')
+    lineHeight(diff)
+    renderCanvas()
+}
+
+// new line not done
+
+function onnewLine() {
+    // console.log('hello newLine')
+    newLine()
+    clearTxtInput()
+    renderCanvas()
+}
+
+function clearTxtInput() {
+    // console.log('hello clearTxtInput')
+    document.querySelector('.text-input').value = ''
+}
+
+function getCurrLine() {
+    gCurrLine = getMemeCurrLine()
+    console.log('hello getCurrLine', gCurrLine)
+}
 
 // -------- CLEAR CANVAS ---------- //
 
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -6,29 +6,80 @@ var gMeme = {
     selectedLineIdx: 0,
     lines: [{
         txt: gElText.value,
-        size: 20,
-        align: 'left',
-        color: 'red'
-    }]
+        size: 40,
+        align: 'center',
+        margin: 'black',
+        color: 'white',
+        font: 'impact',
+        lineHeight: 50
+    }, ]
 }
 
 
 function getMemeForDisplay() {
-    console.log('hello getMemeForDisplay')
+    // console.log('hello getMemeForDisplay')
     const meme = gMeme
     return meme
 }
 
+
+// ---------- TEXT -----------
+
+
 function updateMemeText() {
-    console.log('hello updateMemeText')
+    // console.log('hello updateMemeText')
     var elText = document.querySelector('.text-input').value
-    console.log('elText', elText);
-    gMeme.lines[0].txt = elText
+        // console.log('elText', elText);
+    gMeme.lines[gMeme.selectedLineIdx].txt = elText
     renderCanvas()
 }
 
-function updateMemePic(elImg) { // bug text wont clear
-    console.log('hello updateMemeText', elImg.id)
+function fontSize(diff) {
+    // console.log('hello fontSize')
+    gMeme.lines[0].size += diff
+        // console.log('gMeme.lines[0].size', gMeme.lines[0].size);
+}
+
+function lineHeight(diff) {
+    // console.log('hello lineHeight')
+    gMeme.lines[0].lineHeight += diff
+}
+
+function newLine() {
+    // console.log('hello newLine')
+    if (gMeme.lines.length < 2) { createLine() }
+    (gMeme.selectedLineIdx === 0) ? gMeme.selectedLineIdx = 1: gMeme.selectedLineIdx = 0
+    getCurrLine()
+    gMeme.lines[gCurrLine].txt = gElText.value
+        // console.log('gCurrLine', gCurrLine);
+}
+
+function createLine() {
+    // console.log('hello createLine')
+    const line = {
+        txt: gElText.value,
+        size: 40,
+        align: 'center',
+        margin: 'black',
+        color: 'white',
+        font: 'impact',
+        lineHeight: 450
+    }
+    gMeme.lines.push(line)
+        // console.log('gMeme', gMeme);
+}
+
+function getMemeCurrLine() {
+    // console.log('hello getMemeCurrLine')
+    return gMeme.selectedLineIdx
+}
+
+//----------------
+
+function updateMemePic(elImg) {
+    clearTxtInput()
+    gMeme.lines[gCurrLine].txt = ''
+        // console.log('hello updateMemeText', elImg.id)
     gMeme.selectedImgId = elImg.id
     renderCanvas()
 }
